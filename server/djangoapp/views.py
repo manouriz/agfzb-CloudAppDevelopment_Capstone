e84df0e884e05d3bc0d8404d10510f725eb973b1
Manouchehr <nanochipset@gmail.com>
2021-09-14 18:35:51 +0000

    Lab2 is done!

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -19,35 +19,43 @@ def template_01(request):
     context = {}
     return render(request, 'djangoapp/template-01.html', context)
 
-def about(request):
+# Create an `about` view to render a static about page
+def about_request(request):
     context = {}
     return render(request, 'djangoapp/about.html', context)
 
-def contact(request):
+# Create a `contact` view to return a static contact page
+def contact_request(request):
     context = {}
     return render(request, 'djangoapp/contact.html', context)
 
-
-
-# Create an `about` view to render a static about page
-# def about(request):
-# ...
-
-
-# Create a `contact` view to return a static contact page
-#def contact(request):
-
 # Create a `login_request` view to handle sign in request
-# def login_request(request):
-# ...
+def login_request(request):
+    context = {}
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['psw']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('djangoapp:index')
+        else:
+            context['message'] = "Invalid username or password."
+            return render(request, 'djangoapp/login.html', context)
+    else:
+        return render(request, 'djangoapp/login.html', context)
 
 # Create a `logout_request` view to handle sign out request
-# def logout_request(request):
-# ...
+def logout_request(request):
+    logout(request)
+    return redirect('djangoapp:index')
 
 # Create a `registration_request` view to handle sign up request
-# def registration_request(request):
-# ...
+def registration_request(request):
+    context = {}
+    return render(request, 'djangoapp/registration.html', context)
+
+
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
