6ac098e5be6b294fb3f77c9fe18ed7b73e20b0e6
Manouchehr <nanochipset@gmail.com>
2021-09-15 18:34:24 +0000

    api v1

--- functions/sample/nodejs/index.js
+++ functions/sample/nodejs/index.js
@@ -5,20 +5,23 @@
 const Cloudant = require('@cloudant/cloudant');
 
 function main(params) {
-
     const cloudant = Cloudant({
         url: params.COUCH_URL,
         plugins: { iamauth: { iamApiKey: params.IAM_API_KEY } }
     });
 
-    let dbList = getDbs(cloudant);
-    return { dbs: dbList };
+    let dbListPromise = getDbs(cloudant);
+    return dbListPromise;
 }
 
 function getDbs(cloudant) {
-    cloudant.db.list().then((body) => {
-        body.forEach((db) => {
-            dbList.push(db);
-        });
-    }).catch((err) => { console.log(err); });
+    return new Promise((resolve, reject) => {
+        cloudant.db.list()
+            .then(body => {
+                resolve({ dbs: body });
+            })
+            .catch(err => {
+                reject({ err: err });
+            });
+    });
 }
